e2907466de87f74bbfd7b702d71af539c99e7300
YYYurz <1228396352@qq.com>
2022-01-29 19:38:05 +0800

    (o゜▽゜)o☆

---
+++ BiuBiu/Assets/GameMain/Editor/PathfindingTool/MapCellType.cs
@@ -0,0 +1,26 @@
+﻿//------------------------------------------------------------
+// AureFramework
+// Developed By ZhiRui Yu.
+// GitHub: https://github.com/YYYurz
+// Gitee: https://gitee.com/yyyurz
+// Email: 1228396352@qq.com
+//------------------------------------------------------------
+
+namespace BiuBiu.Editor
+{
+	/// <summary>
+	/// 地图格子类型
+	/// </summary>
+	public enum MapCellType
+	{
+		/// <summary>
+		/// 空，也就是空气墙
+		/// </summary>
+		None,
+		
+		/// <summary>
+		/// 普通可行走地面
+		/// </summary>
+		NormalFloor,
+	}
+}
